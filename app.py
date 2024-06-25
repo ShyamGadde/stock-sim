f97c79dfaca671dfa4a994e89ce52ad1cb1ffa54
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-25 09:34:45 +0530

    refactor: organize imports

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import os
 from cs50 import SQL
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flask_session import Session
 from helpers import apology, login_required, lookup, usd
 
 load_dotenv()
